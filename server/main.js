9e81c79e71d14a780558917b1507fa6d6536441e
Daniel Spitzer <dsp@tradeshift.com>
2011-11-10 14:32:38 +0100

    fixed static file serving

--- server/main.js
+++ server/main.js
@@ -26,7 +26,7 @@ exports.createServer = function(port,db,es){
 					res.writeHead(200,{'Content-Type':'image/vnd.microsoft.icon'});
 					res.end(data);
 				});
-			} else if ( /\/(Storage|Utils|Client|Ui|Core|plug|jq|fn).js/.test(req.url) ) {
+			} else if ( /\/(storage|utils|client|ui|core|plug|jq|fn).js/.test(req.url) ) {
 				fs.readFile('client/js'+req.url,'utf-8',function(err,data){
 					res.writeHead(200,{'Content-Type':'application/javascript'});
 					res.end(data);
